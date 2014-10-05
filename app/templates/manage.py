ee8d71321fb18233dd9657bb3eb67b6cbc5a4bf6
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-10-05 21:17:52 +0800

    generate project name in index.js and then use plain function in templatel.

--- app/templates/manage.py
+++ app/templates/manage.py
@@ -7,6 +7,6 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<%= _.slugify(projectName) %>.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<%= projectName %>.settings.dev")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
