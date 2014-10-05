9fdb3609fde524dcf528b6024fc5b68834255448
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-10-05 21:16:58 +0800

    add more settings

--- app/templates/project/settings/dev.py
+++ app/templates/project/settings/dev.py
@@ -1,3 +1,12 @@
 from .project import *
 
 ENVIRONMENT_NAME = "dev"
+
+INSTALLED_APPS += (
+    'debug_toolbar.apps.DebugToolbarConfig',
+)
+
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
--- app/templates/project/settings/project.py
+++ app/templates/project/settings/project.py
@@ -2,3 +2,14 @@
 from .core import *
 
 ENVIRONMENT_NAME = "project"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': op.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '<%= projectName %>',
+        'PASSWORD': '<%= projectName %>',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
+}
